e5696f134723feb8df77ca576f1184fd4e328cc1
ahhzaky <siahmadzaky@gmail.com>
2020-08-26 11:04:37 +0700

    Add picture api tmdb and size

--- src/App.js
+++ src/App.js
@@ -7,8 +7,14 @@ function App() {
   return (
     <div className="App">
       <h1>hello jack</h1>
-      <ROW title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <ROW title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}/>
       <ROW title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <ROW title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <ROW title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <ROW title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <ROW title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <ROW title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <ROW title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
     </div>
   );
 };
--- src/Row/Row.js
+++ src/Row/Row.js
@@ -1,9 +1,10 @@
 import React, { useState,  useEffect } from 'react';
 import axios from "../Requests/axios";
+import './Row.css';
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
-function Row({ title,fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
 
     const [movies, setMovies] = useState([]);
     
@@ -25,9 +26,8 @@ function Row({ title,fetchUrl }) {
             <h2>{title}</h2>
 
             <div className="row__posters">
-
                 {movies.map(movie => (
-                    <img className="row__poster" src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                    <img key={movie.id} className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}/>
                 ))}
             </div>
         </div>
